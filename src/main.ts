1c41e7d940a67e5f9783dc138609882919671679
Manuel Simon <info@der-supernerd.de>
2025-03-04 11:22:07 +0100

    TS functions

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import './style.css'
 
 let age:number = 43;
 
-for (let i = 0; i <= age; i++) {
-    console.log(i);
+for (let i = 0; i < age; i++) {
+    console.log(i+1);
 }
 
 if(age >= 18) {
@@ -15,7 +15,7 @@ if(age >= 18) {
 
 let score:number = 0;
 
-if(score != 0) {
+if(score !== 0) {
     console.log("Score is available");
 }
 
@@ -39,3 +39,29 @@ if(isAdmin) {
 } else {
     console.log("isAdmin is false.");
 }
+
+const numbers: number[] = [
+    3, 7, 12, 25, 39, 42, 56, 63, 78, 81,
+    90, 105, 119, 134, 150, 167, 182, 199, 210, 225
+];
+const newNumbers = numbers.map(number => number * 2);
+console.log(newNumbers);
+
+const words: string[] = [
+    "apple", "banana", "grape", "orange", "cherry",
+    "mountain", "river", "ocean", "sunset", "morning",
+    "keyboard", "monitor", "speaker", "printer", "battery",
+    "sunshine", "notebook", "backpack", "journey", "holiday"
+];
+
+console.log(words);
+const filteredWords: string[] = words.filter(word => word.length > 5);
+console.log(filteredWords);
+
+
+const sum: number = numbers.reduce((acc, num) => acc + num, 0);
+console.log(`Die Summe aller Zahlen ist: ${sum}`);
+
+const newNumbers2: boolean = numbers.some(number => number > 10);
+console.log(newNumbers2);
+
